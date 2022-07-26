e5474bfcfb1df4f275ed9ce69224a6b11b2c78f7
Hernan Gryszkiewicz <rokoposter@yahoo.com.ar>
2022-07-26 20:08:04 -0300

    back end
    Carpeta Backend

---
+++ ArgentinaProgramaFinal_Hernan_Gryszkiewicz/BackEnd/src/test/java/com/integrador/HRG/HrgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.integrador.HRG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
